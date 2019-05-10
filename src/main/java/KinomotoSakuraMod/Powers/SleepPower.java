4a17e59a725cfd8a53bfa2d6a99a4989541030d0
tuzishu <477630080@qq.com>
2019-05-10 13:17:23 +0800

    修复 “睡”牌可用

--- src/main/java/KinomotoSakuraMod/Powers/SleepPower.java
+++ src/main/java/KinomotoSakuraMod/Powers/SleepPower.java
@@ -5,14 +5,10 @@ import com.megacrit.cardcrawl.actions.common.HealAction;
 import com.megacrit.cardcrawl.actions.common.ReducePowerAction;
 import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
 import com.megacrit.cardcrawl.cards.DamageInfo;
-import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.PowerStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import com.megacrit.cardcrawl.monsters.EnemyMoveInfo;
-
-import java.lang.reflect.Field;
 
 public class SleepPower extends CustomPower
 {
@@ -21,9 +17,6 @@ public class SleepPower extends CustomPower
     public static final String[] POWER_DESCRIPTIONS;
     private static final String POWER_IMG_PATH = "img/powers/default_power.png";
     private static final PowerType POWER_TYPE = PowerType.DEBUFF;
-    private byte moveByte;
-    private AbstractMonster.Intent moveIntent;
-    private EnemyMoveInfo move;
 
     static
     {
@@ -42,50 +35,32 @@ public class SleepPower extends CustomPower
     public void updateDescription()
     {
         this.description = POWER_DESCRIPTIONS[0] + this.amount + POWER_DESCRIPTIONS[1];
-
-    }
-
-    public void atEndOfRound()
-    {
-        if (this.amount <= 0)
-        {
-            AbstractDungeon.actionManager.addToBottom(new RemoveSpecificPowerAction(this.owner, this.owner, this));
-        }
-        else
-        {
-            AbstractDungeon.actionManager.addToBottom(new ReducePowerAction(this.owner, this.owner, this, 1));
-        }
-
     }
 
     public void onInitialApplication()
     {
-        AbstractDungeon.actionManager.addToBottom(new AbstractGameAction()
+        DoStunAction();
+    }
+
+    public void atEndOfRound()
+    {
+        if (this.amount > 1)
         {
-            public void update()
-            {
-                if (SleepPower.this.owner instanceof AbstractMonster)
-                {
-                    SleepPower.this.moveByte = ((AbstractMonster) SleepPower.this.owner).nextMove;
-                    SleepPower.this.moveIntent = ((AbstractMonster) SleepPower.this.owner).intent;
+            DoStunAction();
+            AbstractDungeon.actionManager.addToBottom(new ReducePowerAction(this.owner, this.owner, this, 1));
+        }
+        else
+        {
+            AbstractDungeon.actionManager.addToTop(new RemoveSpecificPowerAction(this.owner, this.owner, this));
+            AbstractDungeon.actionManager.addToTop(new HealAction(this.owner, this.owner, this.owner.maxHealth - this.owner.currentHealth));
+        }
+    }
 
-                    try
-                    {
-                        Field f = AbstractMonster.class.getDeclaredField("move");
-                        f.setAccessible(true);
-                        SleepPower.this.move = (EnemyMoveInfo) f.get(SleepPower.this.owner);
-                        SleepPower.this.move.intent = AbstractMonster.Intent.SLEEP;
-                        ((AbstractMonster) SleepPower.this.owner).createIntent();
-                    }
-                    catch (NoSuchFieldException | IllegalAccessException var2)
-                    {
-                        var2.printStackTrace();
-                    }
-                }
-
-                this.isDone = true;
-            }
-        });
+    public float atDamageReceive(float damage, DamageInfo.DamageType damageType)
+    {
+        AbstractDungeon.actionManager.addToTop(new RemoveSpecificPowerAction(this.owner, this.owner, this));
+        AbstractDungeon.actionManager.addToTop(new HealAction(this.owner, this.owner, this.owner.maxHealth - this.owner.currentHealth));
+        return damage;
     }
 
     public void onRemove()
@@ -93,23 +68,26 @@ public class SleepPower extends CustomPower
         if (this.owner instanceof AbstractMonster)
         {
             AbstractMonster monster = (AbstractMonster) this.owner;
-            if (this.move != null)
-            {
-                monster.setMove(this.moveByte, this.moveIntent, this.move.baseDamage, this.move.multiplier, this.move.isMultiDamage);
-            }
-            else
-            {
-                monster.setMove(this.moveByte, this.moveIntent);
-            }
-
+            monster.rollMove();
             monster.createIntent();
             monster.applyPowers();
-            AbstractDungeon.actionManager.addToBottom(new HealAction(monster, monster, monster.maxHealth - monster.currentHealth));
         }
     }
 
-    public void onAttack(DamageInfo info, int damageAmount, AbstractCreature target)
+    private void DoStunAction()
     {
-        AbstractDungeon.actionManager.addToBottom(new RemoveSpecificPowerAction(this.owner, this.owner, this));
+        AbstractDungeon.actionManager.addToBottom(new AbstractGameAction()
+        {
+            public void update()
+            {
+                SleepPower self = SleepPower.this;
+                if (self.owner instanceof AbstractMonster)
+                {
+                    ((AbstractMonster) self.owner).setMove((byte) -1, AbstractMonster.Intent.SLEEP);
+                    ((AbstractMonster) self.owner).createIntent();
+                }
+                this.isDone = true;
+            }
+        });
     }
 }
