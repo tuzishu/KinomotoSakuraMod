61958cbf38be901d13163274e6b99779799d1f1e
tuzishu-home <477630080@qq.com>
2019-05-14 03:08:20 +0800

    添加 光暗属性

---
+++ src/main/java/KinomotoSakuraMod/Powers/LightMagickPower.java
@@ -0,0 +1,44 @@
+package KinomotoSakuraMod.Powers;
+
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.PowerStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+public class LightMagickPower extends CustomPower
+{
+    public static final String POWER_ID = "LightMagickPower";
+    private static final String POWER_NAME;
+    private static final String[] POWER_DESCRIPTIONS;
+    private static final String POWER_IMG_PATH = "img/powers/default_power.png";
+    private static final PowerType POWER_TYPE = PowerType.BUFF;
+
+    static
+    {
+        PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
+        POWER_NAME = powerStrings.NAME;
+        POWER_DESCRIPTIONS = powerStrings.DESCRIPTIONS;
+    }
+
+    public LightMagickPower(AbstractCreature target, int amount)
+    {
+        super(POWER_ID, POWER_NAME, POWER_IMG_PATH, POWER_TYPE, target, amount);
+        this.updateDescription();
+    }
+
+    public void updateDescription()
+    {
+        this.description = POWER_DESCRIPTIONS[0] + this.amount + POWER_DESCRIPTIONS[1] + this.amount + POWER_DESCRIPTIONS[2];
+    }
+
+    public void atStartOfTurn()
+    {
+        for (AbstractMonster monster : AbstractDungeon.getMonsters().monsters)
+        {
+            AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(monster, this.owner, new LightElementPower(monster, this.amount), this.amount));
+        }
+        AbstractDungeon.player.gameHandSize = AbstractDungeon.player.masterHandSize + this.amount;
+    }
+}
