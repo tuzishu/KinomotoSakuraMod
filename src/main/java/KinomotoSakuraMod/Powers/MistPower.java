0374b7904d6caca5d4a36f63b226b78dfc3a4c31
tuzishu-home <477630080@qq.com>
2019-05-16 23:51:38 +0800

    添加 “雾”牌

---
+++ src/main/java/KinomotoSakuraMod/Powers/MistPower.java
@@ -0,0 +1,52 @@
+package KinomotoSakuraMod.Powers;
+
+import com.megacrit.cardcrawl.actions.common.ReducePowerAction;
+import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.PowerStrings;
+
+public class MistPower extends CustomPower
+{
+    public static final String POWER_ID = "MistPower";
+    private static final String POWER_NAME;
+    private static final String[] POWER_DESCRIPTIONS;
+    private static final String POWER_IMG_PATH = "img/powers/default_power.png";
+    private static final PowerType POWER_TYPE = PowerType.DEBUFF;
+
+    static
+    {
+        PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
+        POWER_NAME = powerStrings.NAME;
+        POWER_DESCRIPTIONS = powerStrings.DESCRIPTIONS;
+    }
+
+    public MistPower(AbstractCreature target, int amount)
+    {
+        super(POWER_ID, POWER_NAME, POWER_IMG_PATH, POWER_TYPE, target, amount);
+        this.updateDescription();
+    }
+
+    public void updateDescription()
+    {
+        this.description = POWER_DESCRIPTIONS[0];
+    }
+
+    public void atEndOfTurn(boolean isPlayer)
+    {
+        if (this.amount > 1)
+        {
+            AbstractDungeon.actionManager.addToTop(new ReducePowerAction(this.owner, this.owner, this, 1));
+        }
+        else
+        {
+            AbstractDungeon.actionManager.addToTop(new RemoveSpecificPowerAction(this.owner, this.owner, this));
+        }
+    }
+
+    public float modifyBlock(float blockAmount)
+    {
+        return 0;
+    }
+}
