cd42eb535fdfdf2d90b841eab9d3ff9244dba2c9
tuzishu-home <477630080@qq.com>
2019-06-14 23:22:43 +0800

    回

--- src/main/java/KinomotoSakuraMod/Cards/ClowCard/ClowCardTheReturn.java
+++ src/main/java/KinomotoSakuraMod/Cards/ClowCard/ClowCardTheReturn.java
@@ -1,8 +1,9 @@
 package KinomotoSakuraMod.Cards.ClowCard;
 
-import KinomotoSakuraMod.Actions.KSMOD_ReturnAction;
 import KinomotoSakuraMod.Cards.KSMOD_AbstractMagicCard;
 import KinomotoSakuraMod.Patches.KSMOD_CustomCardColor;
+import KinomotoSakuraMod.Powers.KSMOD_ReturnPower;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
@@ -14,27 +15,27 @@ public class ClowCardTheReturn extends KSMOD_AbstractMagicCard
     public static final String ID = "ClowCardTheReturn";
     private static final String NAME;
     private static final String DESCRIPTION;
+    private static final String UPGRADE_DESCRIPTION;
     private static final String IMAGE_PATH = "img/cards/clowcard/the_return.png";
-    private static final int COST = 1;
-    private static final CardType CARD_TYPE = CardType.SKILL;
+    private static final int COST = 2;
+    private static final CardType CARD_TYPE = CardType.POWER;
     private static final CardColor CARD_COLOR = KSMOD_CustomCardColor.CLOWCARD_COLOR;
-    private static final CardRarity CARD_RARITY = CardRarity.UNCOMMON;
-    private static final CardTarget CARD_TARGET = CardTarget.NONE;
-    private static final int BASE_MAGIC_NUMBER = 1;
-    private static final int UPGRADE_MAGIC_NUMBER = 1;
+    private static final CardRarity CARD_RARITY = CardRarity.RARE;
+    private static final CardTarget CARD_TARGET = CardTarget.SELF;
+    private static final int BASE_MAGIC_NUMBER = 3;
 
     static
     {
         CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
         NAME = cardStrings.NAME;
         DESCRIPTION = cardStrings.DESCRIPTION;
+        UPGRADE_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
     }
 
     public ClowCardTheReturn()
     {
         super(ID, NAME, IMAGE_PATH, COST, DESCRIPTION, CARD_TYPE, CARD_COLOR, CARD_RARITY, CARD_TARGET);
         this.setBaseMagicNumber(BASE_MAGIC_NUMBER);
-        this.exhaust = true;
     }
 
     public KSMOD_AbstractMagicCard makeCopy()
@@ -47,13 +48,15 @@ public class ClowCardTheReturn extends KSMOD_AbstractMagicCard
         if (!this.upgraded)
         {
             this.upgradeName();
-            this.upgradeMagicNumber(UPGRADE_MAGIC_NUMBER);
+            this.retain = true;
+            this.rawDescription = UPGRADE_DESCRIPTION;
+            this.initializeDescription();
         }
     }
 
     @Override
-    public void use(AbstractPlayer player, AbstractMonster monster)
+    public void applyNormalEffect(AbstractPlayer player, AbstractMonster monster)
     {
-        AbstractDungeon.actionManager.addToBottom(new KSMOD_ReturnAction(this.magicNumber));
+        AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(player, player, new KSMOD_ReturnPower(player, this.magicNumber)));
     }
 }
---
+++ src/main/java/KinomotoSakuraMod/Powers/KSMOD_ReturnPower.java
@@ -0,0 +1,67 @@
+package KinomotoSakuraMod.Powers;
+
+import com.megacrit.cardcrawl.actions.common.*;
+import com.megacrit.cardcrawl.actions.unique.RemoveAllPowersAction;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.PowerStrings;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+
+import java.util.ArrayList;
+
+public class KSMOD_ReturnPower extends KSMOD_CustomPower
+{
+    public static final String POWER_ID = "KSMOD_ReturnPower";
+    private static final String POWER_NAME;
+    private static final String[] POWER_DESCRIPTIONS;
+    private static final String POWER_IMG_PATH = "img/powers/default_power.png";
+    private static final PowerType POWER_TYPE = PowerType.BUFF;
+    private ArrayList<AbstractPower> powerList = new ArrayList<>();
+    private int recordHP;
+    private int recordBlock;
+
+    static
+    {
+        PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
+        POWER_NAME = powerStrings.NAME;
+        POWER_DESCRIPTIONS = powerStrings.DESCRIPTIONS;
+    }
+
+    public KSMOD_ReturnPower(AbstractCreature target, int amount)
+    {
+        super(POWER_ID, POWER_NAME, POWER_IMG_PATH, POWER_TYPE, target, amount);
+        this.updateDescription();
+    }
+
+    public void updateDescription()
+    {
+        this.description = POWER_DESCRIPTIONS[0] + this.amount + POWER_DESCRIPTIONS[1];
+    }
+
+    public void onInitialApplication()
+    {
+        this.powerList = (ArrayList<AbstractPower>) this.owner.powers.clone();
+        this.recordHP = this.owner.currentHealth;
+        this.recordBlock = this.owner.currentBlock;
+    }
+
+    public void atStartOfTurn()
+    {
+        if (this.amount > 1)
+        {
+            AbstractDungeon.actionManager.addToBottom(new ReducePowerAction(this.owner, this.owner, this, 1));
+        }
+        else
+        {
+            AbstractDungeon.actionManager.addToBottom(new RemoveAllPowersAction(this.owner, false));
+            for (AbstractPower power : this.powerList)
+            {
+                AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(this.owner, this.owner, power));
+            }
+            AbstractDungeon.actionManager.addToBottom(new HealAction(this.owner, this.owner, this.recordHP - this.owner.currentHealth));
+            AbstractDungeon.actionManager.addToBottom(new RemoveAllBlockAction(this.owner, this.owner));
+            AbstractDungeon.actionManager.addToBottom(new GainBlockAction(this.owner, this.owner, this.recordBlock));
+        }
+    }
+}
