26fec33433fcd30356f235f555e1c2d27e19614e
tuzishu-home <477630080@qq.com>
2019-07-07 13:40:58 +0800

    增加 封牌 伤害

--- src/main/java/KinomotoSakuraMod/Cards/SpellCard/SpellCardSeal.java
+++ src/main/java/KinomotoSakuraMod/Cards/SpellCard/SpellCardSeal.java
@@ -24,7 +24,7 @@ public class SpellCardSeal extends KSMOD_AbstractSpellCard
     private static final CardRarity CARD_RARITY = CardRarity.BASIC;
     private static final CardTarget CARD_TARGET = CardTarget.ENEMY;
     private static final int BASE_DAMAGE = 10;
-    private static final int UPGRADE_DAMAGE = 4;
+    private static final int UPGRADE_DAMAGE = 6;
     private static final int BASE_MAGIC_NUMBER = 2;
 
     static
