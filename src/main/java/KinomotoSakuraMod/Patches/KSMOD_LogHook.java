e5397547c493b032d9403c5993f20fe666806ec3
tuzishu-laptop <477630080@qq.com>
2020-07-07 10:44:52 +0800

    debug

--- src/main/java/KinomotoSakuraMod/Patches/KSMOD_LogHook.java
+++ src/main/java/KinomotoSakuraMod/Patches/KSMOD_LogHook.java
@@ -1,7 +1,7 @@
 package KinomotoSakuraMod.Patches;
 
-import KinomotoSakuraMod.Utility.KSMOD_Utility;
-import com.badlogic.gdx.graphics.Texture;
+import KinomotoSakuraMod.Utility.KSMOD_LoggerTool;
+import KinomotoSakuraMod.Utility.KSMOD_ReflectTool;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.evacipated.cardcrawl.modthespire.lib.*;
 import com.megacrit.cardcrawl.powers.AbstractPower;
@@ -10,27 +10,25 @@ import javassist.CtBehavior;
 
 public class KSMOD_LogHook
 {
-    // @SpirePatch(clz = FlashPowerEffect.class, method = SpirePatch.CONSTRUCTOR, paramtypez = {
-    //         AbstractPower.class
-    // })
-    // public static class Log
-    // {
-    //     @SpireInsertPatch(locator = Locator.class)
-    //     public static void Insert(FlashPowerEffect effect, AbstractPower power) throws NoSuchFieldException, IllegalAccessException
-    //     {
-    //         TextureAtlas.AtlasRegion img = (TextureAtlas.AtlasRegion) KSMOD_Utility.GetFieldByReflect(FlashPowerEffect.class, "region128").get(effect);
-    //         KSMOD_Utility.Logger.info(power.ID);
-    //         KSMOD_Utility.Logger.info(power.region128);
-    //     }
-    // }
-    //
-    // private static class Locator extends SpireInsertLocator
-    // {
-    //     public int[] Locate(CtBehavior ctMethodToPatch) throws Exception
-    //     {
-    //         Matcher finalMatcher = new Matcher.FieldAccessMatcher(FlashPowerEffect.class, "region128");
-    //         int[] loc = LineFinder.findAllInOrder(ctMethodToPatch, finalMatcher);
-    //         return new int[] {loc[0]};
-    //     }
-    // }
+//    @SpirePatch(clz = FlashPowerEffect.class, method = SpirePatch.CONSTRUCTOR, paramtypez = {AbstractPower.class})
+//    public static class Log
+//    {
+//        @SpireInsertPatch(locator = Locator.class)
+//        public static void Insert(FlashPowerEffect effect, AbstractPower power) throws NoSuchFieldException, IllegalAccessException
+//        {
+//            TextureAtlas.AtlasRegion img = (TextureAtlas.AtlasRegion) KSMOD_ReflectTool.GetFieldByReflect(FlashPowerEffect.class, "region128").get(effect);
+//            KSMOD_LoggerTool.Logger.info(power.ID);
+//            KSMOD_LoggerTool.Logger.info(power.region128);
+//        }
+//    }
+//
+//    private static class Locator extends SpireInsertLocator
+//    {
+//        public int[] Locate(CtBehavior ctMethodToPatch) throws Exception
+//        {
+//            Matcher finalMatcher = new Matcher.FieldAccessMatcher(FlashPowerEffect.class, "region128");
+//            int[] loc = LineFinder.findAllInOrder(ctMethodToPatch, finalMatcher);
+//            return new int[]{loc[0]};
+//        }
+//    }
 }
