c3f1b286a57974b745734ef0b4923764af0ce85c
tuzishu-laptop <477630080@qq.com>
2020-07-21 12:37:27 +0800

    重做飞：0费，抽2张牌（升级前：消耗4层魔力充能）

--- src/main/java/KinomotoSakuraMod/Cards/ClowCard/ClowCardTheFly.java
+++ src/main/java/KinomotoSakuraMod/Cards/ClowCard/ClowCardTheFly.java
@@ -4,36 +4,41 @@ import KinomotoSakuraMod.Cards.KSMOD_AbstractMagicCard;
 import KinomotoSakuraMod.Cards.SakuraCard.SakuraCardTheFly;
 import KinomotoSakuraMod.Patches.KSMOD_CustomCardColor;
 import KinomotoSakuraMod.Patches.KSMOD_CustomTag;
+import KinomotoSakuraMod.Powers.KSMOD_MagickChargePower;
 import KinomotoSakuraMod.Relics.KSMOD_SealedBook;
 import com.megacrit.cardcrawl.actions.common.DrawCardAction;
 import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.actions.common.ReducePowerAction;
+import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.AbstractPower;
 
 public class ClowCardTheFly extends KSMOD_AbstractMagicCard
 {
     public static final String ID = "ClowCardTheFly";
     private static final String NAME;
     private static final String DESCRIPTION;
+    private static final String UPGRADE_DESCRIPTION;
     private static final String[] EXTENDED_DESCRIPTION;
     private static final String IMAGE_PATH = "img/cards/clowcard/the_fly.png";
-    private static final int COST = 1;
+    private static final int COST = 0;
     private static final CardType CARD_TYPE = CardType.SKILL;
     private static final CardColor CARD_COLOR = KSMOD_CustomCardColor.CLOWCARD_COLOR;
     private static final CardRarity CARD_RARITY = CardRarity.UNCOMMON;
-    private static final CardTarget CARD_TARGET = CardTarget.SELF;
-    private static final int BASE_BLOCK = 5;
+    private static final CardTarget CARD_TARGET = CardTarget.NONE;
     private static final int BASE_MAGIC_NUMBER = 2;
-    private static final int UPGRADE_MAGIC_NUMBER = 1;
+    private static final int MAGICK_CHARGE_COST = 4;
 
     static
     {
         CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
         NAME = cardStrings.NAME;
         DESCRIPTION = cardStrings.DESCRIPTION;
+        UPGRADE_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
         EXTENDED_DESCRIPTION = cardStrings.EXTENDED_DESCRIPTION;
     }
 
@@ -42,7 +47,6 @@ public class ClowCardTheFly extends KSMOD_AbstractMagicCard
         super(ID, NAME, IMAGE_PATH, COST, DESCRIPTION, CARD_TYPE, CARD_COLOR, CARD_RARITY, CARD_TARGET, true);
         this.tags.add(KSMOD_CustomTag.KSMOD_WINDY_CARD);
         this.cardsToPreview = new SakuraCardTheFly();
-        this.baseBlock = BASE_BLOCK;
         this.setBaseMagicNumber(BASE_MAGIC_NUMBER);
     }
 
@@ -56,22 +60,35 @@ public class ClowCardTheFly extends KSMOD_AbstractMagicCard
         if (!this.upgraded)
         {
             this.upgradeName();
-            this.upgradeMagicNumber(UPGRADE_MAGIC_NUMBER);
+            this.rawDescription = UPGRADE_DESCRIPTION;
+            this.initializeDescription();
         }
     }
 
     @Override
     public void applyNormalEffect(AbstractPlayer player, AbstractMonster monster)
     {
-        // AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(player, player, new KSMOD_FlyPower_SakuraCard(this.magicNumber), this.magicNumber));
-        AbstractDungeon.actionManager.addToBottom(new GainBlockAction(player, player, this.block));
         AbstractDungeon.actionManager.addToBottom(new DrawCardAction(player, this.magicNumber));
+        if (upgraded)
+        {
+            if (player.hasPower(KSMOD_MagickChargePower.POWER_ID))
+            {
+                AbstractPower power = player.getPower(KSMOD_MagickChargePower.POWER_ID);
+                if (power.amount <= MAGICK_CHARGE_COST)
+                {
+                    AbstractDungeon.actionManager.addToBottom(new RemoveSpecificPowerAction(player, player, power));
+                }
+                else
+                {
+                    AbstractDungeon.actionManager.addToBottom(new ReducePowerAction(player, player, power, MAGICK_CHARGE_COST));
+                }
+            }
+        }
     }
 
     @Override
     public void applyExtraEffect(AbstractPlayer player, AbstractMonster monster)
     {
-        AbstractDungeon.actionManager.addToBottom(new GainBlockAction(player, player, this.block));
         AbstractDungeon.actionManager.addToBottom(new DrawCardAction(player, this.magicNumber + KSMOD_SealedBook.DRAW_NUMBER));
     }
 
