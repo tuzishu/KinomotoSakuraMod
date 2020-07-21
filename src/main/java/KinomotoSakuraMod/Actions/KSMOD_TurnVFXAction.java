c56d70554b3a02cd8004c2b7b1b308f47ade71a0
tuzishu-laptop <477630080@qq.com>
2020-07-22 00:03:24 +0800

    优化代码

--- src/main/java/KinomotoSakuraMod/Actions/KSMOD_TurnVFXAction.java
+++ src/main/java/KinomotoSakuraMod/Actions/KSMOD_TurnVFXAction.java
@@ -4,33 +4,24 @@ import KinomotoSakuraMod.Cards.KSMOD_AbstractMagicCard;
 import KinomotoSakuraMod.Effects.Turn.KSMOD_TurningEffect;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.vfx.AbstractGameEffect;
 
 public class KSMOD_TurnVFXAction extends AbstractGameAction
 {
-    private KSMOD_AbstractMagicCard clowCard;
-    private KSMOD_AbstractMagicCard sakuraCard;
+    private AbstractGameEffect effect;
 
     public KSMOD_TurnVFXAction(KSMOD_AbstractMagicCard clowCard, KSMOD_AbstractMagicCard sakuraCard)
     {
-        this.actionType = ActionType.CARD_MANIPULATION;
-        this.startDuration = KSMOD_TurningEffect.GetTotalTime();
-        this.duration = this.startDuration;
-        this.clowCard = clowCard;
-        this.sakuraCard = sakuraCard;
+        effect = new KSMOD_TurningEffect(clowCard, sakuraCard);
+        AbstractDungeon.effectsQueue.add(effect);
     }
 
     @Override
     public void update()
     {
-        if (this.duration == this.startDuration)
-        {
-            AbstractDungeon.effectsQueue.add(new KSMOD_TurningEffect(clowCard, sakuraCard));
-        }
-
-        if (this.duration < 0)
+        if (effect.isDone)
         {
             this.isDone = true;
         }
-        this.tickDuration();
     }
 }
